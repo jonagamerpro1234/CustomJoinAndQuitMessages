4730c1a24a1be2d9dc82ef509ef7c76474eee052
SrJss <31166687+jonagamerpro1234@users.noreply.github.com>
2023-08-30 17:28:31 -0400

    more changes code

--- bukkit/src/main/java/jss/customjoinandquitmessage/listeners/chat/QuitListener.java
+++ bukkit/src/main/java/jss/customjoinandquitmessage/listeners/chat/QuitListener.java
@@ -1,14 +1,35 @@
 package jss.customjoinandquitmessage.listeners.chat;
 
+import jss.customjoinandquitmessage.files.utils.Settings;
+import jss.customjoinandquitmessage.managers.JoinQuitMessageHandlerFactory;
+import jss.customjoinandquitmessage.utils.MessageUtils;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class QuitListener implements Listener {
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent e){
+    private final JoinQuitMessageHandlerFactory handlerFactory = JoinQuitMessageHandlerFactory.getInstance();
 
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void onQuit(@NotNull PlayerQuitEvent e){
+        Player p = e.getPlayer();
+        boolean isNormal = Settings.chatformat_type.equalsIgnoreCase("normal");
+        boolean isGroup = Settings.chatformat_type.equalsIgnoreCase("group");
+
+        handlerFactory.getActiveHandler().welcome(p);
+
+        if(isNormal){
+            e.setQuitMessage(null);
+            handlerFactory.getActiveHandler().handlerJoinAndQuitMessages(p,false);
+            handlerFactory.getActiveHandler().handlerJoinAndQuitTitle(p,false);
+            handlerFactory.getActiveHandler().handlerJoinAndQuitActionbar(p,false );
+        }else if (isGroup) {
+            e.setQuitMessage(null);
+            MessageUtils.sendColorMessage(p,"Test group");
+        }
     }
-
 }
